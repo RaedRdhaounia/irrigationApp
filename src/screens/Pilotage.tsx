581d80fcfac62609b7bcfa8e894e30d8dbab728d
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-02 22:22:22 +0100

    create new screens
    create new screen for the app

---
+++ src/screens/Pilotage.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Pilotage = () => {
+  return (
+    <View>
+      <Text>Pilotage</Text>
+    </View>
+  );
+};
+
+export default Pilotage;
---
+++ src/screens/Wether.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Wether = () => {
+  return (
+    <View>
+      <Text>Wether screen</Text>
+    </View>
+  );
+};
+
+export default Wether;
